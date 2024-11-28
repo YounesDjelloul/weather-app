bc54bf275a12cfcb072a997a83e455c118bde7e9
YounesDjelloul <younesdjelloul14@gmail.com>
2024-11-28 15:16:30 +0800

    profile page header

---
+++ components/Organisms/ProfileHeader.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="profile-header">
+    <NuxtLink to="/">
+      <Icon class="profile-header__back-icon" name="weui:back-outlined"/>
+    </NuxtLink>
+    <div class="profile-header__title">Edit Profile</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.profile-header {
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 1.3rem .5rem;
+  gap: 35%;
+
+  &__back-icon {
+    font-size: 1.3rem;
+    cursor: pointer;
+  }
+
+  &__title {
+    font-size: 1rem;
+    font-weight: 600;
+  }
+}
+</style>
--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
 
+import ProfileHeader from "~/components/Organisms/ProfileHeader.vue";
 </script>
 
 <template>
   <div class="profile">
-    Hello World!
+    <ProfileHeader/>
   </div>
 </template>
 
 <style scoped lang="scss">
-
+.profile {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+}
 </style>
