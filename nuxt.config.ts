116377365ddb1eae961af21eb0ed7f35baab418c
YounesDjelloul <younesdjelloul14@gmail.com>
2024-12-02 02:44:39 +0800

    adjusting app base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
     devtools: {enabled: true},
     modules: ['@nuxt/icon', '@pinia/nuxt', 'pinia-plugin-persistedstate/nuxt'],
     app: {
+        baseURL: '',
         pageTransition: {
             name: 'page',
             mode: 'out-in'
