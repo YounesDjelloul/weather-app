9af5481d46e94108e2f5f6bd0986a13ba0f250e7
YounesDjelloul <younesdjelloul14@gmail.com>
2024-12-02 15:47:58 +0800

    specifying used icon collections

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: {enabled: true},
     modules: ['@nuxt/icon', '@pinia/nuxt', 'pinia-plugin-persistedstate/nuxt'],
+    icon: {
+        serverBundle: {
+            collections: ['uil', 'mdi', 'weui', 'solar', 'eva']
+        }
+    },
     app: {
         baseURL: '/weather-app/',
         buildAssetsDir: '/_nuxt/',
